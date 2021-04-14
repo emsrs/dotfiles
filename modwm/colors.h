b077fc647447dc0df03bd8018d28fecb4c48f881
crysco-byte <cryptoescor@protonmail.com>
2021-04-14 16:23:00 -0300

    added modwm config files

---
+++ modwm/colors.h
@@ -0,0 +1,38 @@
+static char normfgcolor[] = "#81a1c1";
+static char normbgcolor[] = "#2e3440";
+static char normbordercolor[] = "#665C54";
+static char normfloatcolor[] = "#3A3A3A";
+static char selfgcolor[] = "#e5e9f0";
+static char selbgcolor[] = "#322F2E";
+static char selbordercolor[] = "#CC5E50";
+static char selfloatcolor[] = "#FABD2F";
+
+static char titlenormfgcolor[] = "#D3869B";
+static char titlenormbgcolor[] = "#2e3440";
+static char titlenormbordercolor[] = "#444444";
+static char titlenormfloatcolor[] = "#db8fd9";
+
+static char titleselfgcolor[] = "#e5e9f0";
+static char titleselbgcolor[] = "#2e3440";
+static char titleselbordercolor[] = "#005577";
+static char titleselfloatcolor[] = "#005577";
+
+static char tagsnormfgcolor[] = "#e5e9f0";
+static char tagsnormbgcolor[] = "#2e3440";
+static char tagsnormbordercolor[] = "#444444";
+static char tagsnormfloatcolor[] = "#db8fd9";
+
+static char tagsselfgcolor[] = "#88c0d0";
+static char tagsselbgcolor[] = "#3b4252";
+static char tagsselbordercolor[] = "#FABD2F";
+static char tagsselfloatcolor[] = "#FABD2F";
+
+static char hidfgcolor[] = "#81a1c1";
+static char hidbgcolor[] = "#222222";
+static char hidbordercolor[] = "#FABD2F";
+static char hidfloatcolor[] = "#f76e0c";
+
+static char urgfgcolor[] = "#D3869B";
+static char urgbgcolor[] = "#2e3440";
+static char urgbordercolor[] = "#F92672";
+static char urgfloatcolor[] = "#db8fd9";
