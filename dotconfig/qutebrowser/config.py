a125726b2115c630c5d47eaab85894a1aa2187e6
crysco-byte <cryptoescor@protonmail.com>
2021-07-15 20:20:11 -0300

    moved and unbloated config files

---
+++ dotconfig/qutebrowser/config.py
@@ -0,0 +1,9 @@
+config.load_autoconfig(False)
+c.content.javascript.enabled = False
+c.content.private_browsing = True
+c.content.canvas_reading = False
+c.content.geolocation = False
+c.content.desktop_capture = False
+c.content.notifications.enabled = False
+c.content.media.audio_video_capture = False
+c.content.mouse_lock = False
