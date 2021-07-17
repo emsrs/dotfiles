1cdb5e137179db140d4dd03bc3f426a80b989e3b
Your Name <you@example.com>
2021-07-17 16:02:53 -0300

    update qutebrowser config

--- dotconfig/qutebrowser/config.py
+++ dotconfig/qutebrowser/config.py
@@ -1,9 +1,24 @@
 config.load_autoconfig(False)
-c.content.javascript.enabled = False
-c.content.private_browsing = True
-c.content.canvas_reading = False
-c.content.geolocation = False
-c.content.desktop_capture = False
-c.content.notifications.enabled = False
-c.content.media.audio_video_capture = False
-c.content.mouse_lock = False
+
+def privacy():
+        c.content.javascript.enabled = False
+        c.content.private_browsing = True
+        c.content.canvas_reading = False
+        c.content.geolocation = False
+        c.content.desktop_capture = False
+        c.content.notifications.enabled = False
+        c.content.media.audio_video_capture = False
+        c.content.mouse_lock = False
+
+def preferences():
+        c.zoom.default = 140
+        c.fonts.hints = "bold 14pt Iosevka"
+        c.fonts.default_size = "14pt"
+        c.fonts.default_family = "Iosevka"
+        c.colors.hints.bg = "#fff"
+        c.colors.hints.fg = "#000"
+        c.colors.hints.fg = "#000"
+        c.hints.border = "2px solid #000"
+
+privacy()
+preferences()
