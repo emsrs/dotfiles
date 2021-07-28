f14133b541bcc9401442f01e744d1740e86d2671
emsrs <cryptoescor@protonmail.com>
2021-07-28 21:26:35 +0000

    update qutebrowser config

--- dotconfig/qutebrowser/config.py
+++ dotconfig/qutebrowser/config.py
@@ -1,6 +1,9 @@
+
 config.load_autoconfig(False)
 
 def privacy():
+        # If the web site won't load because of javascript
+        # you can enable it on the current host by using the tsh keybinding
         c.content.javascript.enabled = False
         c.content.private_browsing = True
         c.content.canvas_reading = False
@@ -19,6 +22,18 @@ def preferences():
         c.colors.hints.fg = "#000"
         c.colors.hints.fg = "#000"
         c.hints.border = "2px solid #000"
+        c.downloads.location.directory = "~/down"
+        c.tabs.show = "switching"
+        c.statusbar.show = "in-mode"
+
+def minimize_figer_priting():
+        c.content.headers.user_agent = "Mozilla/5.0 (Windows NT 10.0; rv:68.0) Gecko/20100101 Firefox/68.0"
+        c.content.headers.accept_language = "en-US,en;q=0.5"
+        c.content.headers.custom = '{"accept": "text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8"}'
+        c.content.canvas_reading = False
+        c.content.webgl = False
+
+
 
 privacy()
 preferences()
